a4b78f082683fb20bc01d85c1a23ad04e329f44b
Areejslama <areegahmed449@gmail.com>
2024-08-18 02:46:52 +0300

    1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,41 +3,43 @@
 from typing import Tuple
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]  # Exclude the header row
 
         return self.__dataset
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-        """define the function"""
+        """Calculate start and end indices for pagination
+        """
         start = (page - 1) * page_size
         end = start + page_size
-
         return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of data from the dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        assert isinstance(page, int) and page > 0, "page must be more than 0"
-        assert isinstance(page_size, int) and page_size > 0, \
-        "page_size must be more than 0"
+        if self.dataset is None:
+            return []
 
         start, end = self.index_range(page, page_size)
-        data = self.dataset()
-        return data[start:end]
+
+        dataset = self.dataset()
+        return dataset[start:end]
