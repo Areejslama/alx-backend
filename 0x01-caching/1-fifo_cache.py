89218405a333373b00784f9dd7a0dd92c1eeb57c
Areejslama <areegahmed449@gmail.com>
2024-08-21 02:25:16 +0300

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,13 +10,12 @@ class FIFOCache(BaseCaching):
         """ Add an item to the cache """
         if key is None or item is None:
             return
-
-        self.cache_data[key] = item
-
+        
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
             print("DISCARD: ", first_key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Retrieve an item"""
