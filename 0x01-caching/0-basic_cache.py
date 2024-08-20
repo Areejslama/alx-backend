be459c2ff2f7afad39a8014f069bc38e0f43002d
Areejslama <areegahmed449@gmail.com>
2024-08-20 23:23:03 +0300

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,6 +8,8 @@ class BaseCaching():
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
+
+
 class BasicCache(BaseCaching):
     """define class"""
     def __init__(self):
@@ -24,7 +26,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache"""
         self.cache_data[key] = item
-        print("{}: {}".format(key,item))
+        print("{}: {}".format(key, item))
         if key is None or item is None:
             pass
 
