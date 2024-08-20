e09aa9150fc2347a998410769de3e132fa73b4f6
Areejslama <areegahmed449@gmail.com>
2024-08-20 23:45:58 +0300

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,14 +2,16 @@
 """this script define a class"""
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """define class"""
     def put(self, key, item):
         """Add an item in the cache"""
-        if key is None or item is None:
-            pass
         self.cache_data[key] = item
         print("{}: {}".format(key, item))
+
+        if key is None or item is None:
+            pass
 
     def get(self, key):
         """Get an item by key"""
