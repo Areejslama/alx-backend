c98da1d4812c935c2f58327a122c373cc470c5e6
Areejslama <areegahmed449@gmail.com>
2024-08-21 04:33:22 +0300

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,7 +16,7 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 r_key, _ = self.cache_data.popitem(False)
                 print("DISCARD:", r_key)
         self.cache_data[key] = item
