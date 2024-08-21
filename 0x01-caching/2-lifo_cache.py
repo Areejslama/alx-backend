ff40841392bd29e206a7abd0ecc571d9cee679ab
Areejslama <areegahmed449@gmail.com>
2024-08-21 03:42:08 +0300

    lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
-    """ FIFO Cache"""
+    """ LIFO Cache"""
     def __init__(self):
         """intialize"""
         super().__init__()
@@ -15,10 +15,12 @@ class LIFOCache(BaseCaching):
         """Adds an item"""
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key, _ = self.cache_data.popitem(True)
-            print("DISCARD:", last_key)
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """ Retrieve an item"""
