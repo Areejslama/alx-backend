e2dd76202b6de656a089145ca6d85ae66878c2b1
Areejslama <areegahmed449@gmail.com>
2024-08-28 00:19:15 +0300

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,6 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return render_template(0-index.html)
+    """define flask app"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
