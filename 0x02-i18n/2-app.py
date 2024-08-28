a4edbb2e5acd7a5d476777ef465568da4ff1c34b
Areejslama <areegahmed449@gmail.com>
2024-08-28 03:27:35 +0300

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,15 +7,17 @@ from flask_babel import Babel
 class Config:
     """Configuration class for Babel."""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 def get_locale():
-    """Function to select the best match"""
+    """Function to select the best match for supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 babel = Babel(app, locale_selector=get_locale)
 
 
