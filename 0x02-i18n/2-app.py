5c9d684eec57b65db64413d316858fb044336271
Areejslama <areegahmed449@gmail.com>
2024-08-28 03:52:12 +0300

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
-"""This script sets up Babel for localization."""
-from flask import Flask, render_template, request
+"""A Basic Flask app"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
-    """Configuration class for Babel."""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-def get_locale():
-    """Function to select the best match for supported languages."""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Retrieves the locale for a web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def welcome():
-    """Function to render the welcome page."""
+    """The home"""
     return render_template('2-index.html')
 
 
