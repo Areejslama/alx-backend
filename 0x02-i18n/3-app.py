ea6e87b60785bee4f9c1688c58287c70b05095f8
Areejslama <areegahmed449@gmail.com>
2024-08-29 01:54:53 +0300

    translate

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,8 +5,7 @@ from flask_babel import Babel, gettext as _
 
 
 class Config:
-    """define class
-    """
+    """define class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
