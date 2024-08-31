a509182aeecd758095c127278793c04e70fedfc4
Areejslama <areegahmed449@gmail.com>
2024-08-31 04:49:37 +0300

    7

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,1 +1,73 @@
+#!/usr/bin/env python3
+"""A Basic Flask app"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from pytz import timezone
+import pytz.exceptions
 
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Retrieves a user"""
+    login_id = request.args.get('login_as', '')
+    if login_id:
+        return users.get(int(login_id), None)
+    return None
+
+
+@app.before_request
+def before_request():
+    """Performs some routines"""
+    user = get_user()
+    g.user = user
+
+
+
+@babel.localeselector
+def get_locale():
+    """Retrieves the locale"""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """Retrieves the locale"""
+    timezone = request.args.get('timezone')
+    if not timezone and g.user:
+        return g.user['timezone']
+    try:
+        return pytz.timezone(timezone)
+    except pytz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+
+@app.route('/')
+def hello():
+    """The home"""
+    return render_template('7-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
